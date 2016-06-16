1ae72876b6079d3fec0cd5cb751a69806ead7fe2
Joseph Miller <jmnintendo@gmail.com>
2016-06-15 21:23:41 -0700

    Added the beginning of the interface and player input for the hacker side of the game.

---
+++ Assets/_Scripts/HackerTerminal.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HackerTerminal : MonoBehaviour
+{
+	//[SerializeField]
+	//private GameObject
+	// increases the power to the terminal if possible(return true), otherwise can not (return false)
+	public bool IncreasePower()
+	{
+		if ((terminalPower + 1) > maxPower)
+			return false;
+		
+		++terminalPower;
+
+		return true;
+	}
+
+	// decreases the power to the terminal if possible(return true), otherwise can not (return false)
+	public bool DecreasePower()
+	{
+		if ((terminalPower - 1) < 0)
+			return false;
+		
+		--terminalPower;
+
+		return true;
+	}
+
+	[SerializeField]
+	private int maxPower = 3; // maximum amount of allowable power for this terminal
+
+	private int terminalPower = 0; // amount of power allocated to the terminal
+	public int TerminalPower // returns amount of power allocated to the terminal
+	{
+		get
+		{
+			return terminalPower;
+		}
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// kept for possible animation state
+	}
+}
---
+++ Assets/_Scripts/PowerToggle.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerToggle : MonoBehaviour
+{
+	private bool powerOn = false; // is the power on
+	public bool PowerOnStatus // gets and sets the power on status
+	{
+		get
+		{
+			return powerOn;
+		}
+		set
+		{
+			if ( value == true )
+			{
+				powerOn = true;
+				GetComponent<SpriteRenderer>().sprite = powerOnSprite;
+			}
+			else
+			{
+				powerOn = false;
+				GetComponent<SpriteRenderer>().sprite = powerOffSprite;
+			}
+		}
+	}
+
+	[SerializeField]
+	private Sprite powerOnSprite; // the power on sprite
+	[SerializeField]
+	private Sprite powerOffSprite; // the power off sprite
+
+	// Use this for initialization
+	void Start()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+	
+	}
+}
