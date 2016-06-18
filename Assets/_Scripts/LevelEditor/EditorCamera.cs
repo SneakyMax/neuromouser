43ff4bb393291540a283e65726f0b943ad4362cc
Andy Brenneke <sneakymax@gmail.com>
2016-06-18 12:36:47 -0700

    Add a grid background to the level editor.

---
+++ Assets/_Scripts/LevelEditor/EditorCamera.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace Assets._Scripts.LevelEditor
+{
+    [UnityComponent]
+    public class EditorCamera : MonoBehaviour
+    {
+        [AssignedInUnity]
+        public Material LineMaterial;
+
+        [UnityMessage]
+        public void OnPostRender()
+        {
+            var halfSize = new Vector2(
+                Camera.main.orthographicSize * Camera.main.aspect,
+                Camera.main.orthographicSize);
+
+            var leftBounds = (Vector2)Camera.main.transform.position - halfSize;
+            var rightBounds = (Vector2)Camera.main.transform.position + halfSize;
+
+            Debug.Log(leftBounds);
+
+            var gridSize = PlacementGrid.Instance.GridSize;
+            var offset = gridSize / 2f;
+
+            var start = new Vector2(Mathf.Floor(leftBounds.x / gridSize) * gridSize - offset, Mathf.Floor(leftBounds.y / gridSize) * gridSize - offset);
+            var end = new Vector2(Mathf.Ceil(rightBounds.x / gridSize) * gridSize + offset, Mathf.Ceil(rightBounds.y / gridSize + offset) * gridSize + offset);
+
+            for (var x = start.x; x < end.x; x += gridSize)
+            {
+                GL.Begin(GL.LINES);
+                LineMaterial.SetPass(0);
+                GL.Color(LineMaterial.color);
+
+                GL.Vertex3(x, start.y, 0);
+                GL.Vertex3(x, end.y, 0);
+                GL.End();
+            }
+
+            for (var y = start.y; y < end.y; y += gridSize)
+            {
+                GL.Begin(GL.LINES);
+                LineMaterial.SetPass(0);
+                GL.Color(LineMaterial.color);
+
+                GL.Vertex3(start.x, y, 0);
+                GL.Vertex3(end.x, y, 0);
+                GL.End();
+            }
+        } 
+    }
+}
--- Assets/_Scripts/LevelEditor/PlacementGrid.cs
+++ Assets/_Scripts/LevelEditor/PlacementGrid.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Security.AccessControl;
+using UnityEngine;
 
 namespace Assets._Scripts.LevelEditor
 {
