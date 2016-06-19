e0c5167dced759825461b46b26ea1e78ef05f771
Joseph Miller <jmnintendo@gmail.com>
2016-06-19 15:29:45 -0700

    Moved the RunnerMouseInterface.unity to the scenes directory and changed the RunnerPlayer code to use non-variable movement and added facing direction.

--- Assets/_Scripts/RunnerPlayer.cs
+++ Assets/_Scripts/RunnerPlayer.cs
@@ -7,6 +7,29 @@ using System.Collections;
 public class RunnerPlayer : MonoBehaviour
 {
 	/// <summary>
+	/// Current player facing direction.
+	/// </summary>
+	public enum CurrentFacing
+	{
+		Left,
+		Right,
+		Up,
+		Down
+	}
+
+	/// <summary>
+	/// Gets the current direction the mouse is facing.
+	/// </summary>
+	/// <value>Gets the current facing direction.</value>
+	public CurrentFacing CurrentFacingDirection
+	{
+		get
+		{
+			return facing;
+		}
+	}
+
+	/// <summary>
 	/// Gets or sets the current x tile the running player is on.
 	/// </summary>
 	/// <value>The current x.</value>
@@ -44,12 +67,54 @@ public class RunnerPlayer : MonoBehaviour
 	public float RunningSpeed = 2f;
 
 	/// <summary>
-	/// 
+	/// Is the player movement frozen?
+	/// </summary>
+	public bool PlayerMovementFrozen = false;
+
+	/// <summary>
+	/// The direction the player is currently facing.
+	/// </summary>
+	private CurrentFacing facing = CurrentFacing.Up;
+
+	/// <summary>
+	/// Handles the player movement.
+	/// </summary>
+	private void HandlePlayerMovement()
+	{
+		float horizontalAxis = Input.GetAxisRaw("Horizontal");
+		float verticalAxis = Input.GetAxisRaw("Vertical");
+
+		if ( horizontalAxis > float.Epsilon )
+		{
+			horizontalAxis = ( RunningSpeed * Time.deltaTime );
+			facing = CurrentFacing.Right;
+		}
+		else if ( horizontalAxis < -float.Epsilon )
+		{
+			horizontalAxis = ( -RunningSpeed * Time.deltaTime );
+			facing = CurrentFacing.Left;
+		}
+		// vertical facing > horizontal facing
+		if ( verticalAxis > float.Epsilon )
+		{
+			verticalAxis = ( RunningSpeed * Time.deltaTime );
+			facing = CurrentFacing.Up;
+		}
+		else if ( verticalAxis < -float.Epsilon )
+		{
+			verticalAxis = ( -RunningSpeed * Time.deltaTime );
+			facing = CurrentFacing.Down;
+		}
+		GetComponent<Rigidbody2D>().MovePosition(new Vector2((transform.position.x + horizontalAxis),
+                                                             (transform.position.y + verticalAxis)));
+	}
+
+	/// <summary>
+	/// Moves the player and changes the facing direction.
 	/// </summary>
 	private void FixedUpdate()
 	{
-		GetComponent<Rigidbody2D>().MovePosition(
-						new Vector2((transform.position.x + (Input.GetAxis("Horizontal") * RunningSpeed * Time.deltaTime)),
-				        		    (transform.position.y + (Input.GetAxis("Vertical") * RunningSpeed * Time.deltaTime))));
+		if (!PlayerMovementFrozen)
+			HandlePlayerMovement();
 	}
 }
