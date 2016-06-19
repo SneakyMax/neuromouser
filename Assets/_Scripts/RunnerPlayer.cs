ea824cb67083902e7d465a6c81bfafa1af2b0c0c
Joseph Miller <jmnintendo@gmail.com>
2016-06-19 03:05:39 -0700

    Removed old HackerUI.prefab (should have been done last commit). Added basic RunnerPlayer prefab and script support that makes it move and work with collisions (took me way too long and is now very simple...sorry! Still learning!).

---
+++ Assets/_Scripts/RunnerPlayer.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Handles the running player
+/// </summary>
+public class RunnerPlayer : MonoBehaviour
+{
+	/// <summary>
+	/// Gets or sets the current x tile the running player is on.
+	/// </summary>
+	/// <value>The current x.</value>
+	public int CurrentX
+	{
+		get
+		{
+			return Mathf.RoundToInt(transform.position.x);
+		}
+		set
+		{
+			transform.position = new Vector3((float)value, transform.position.y);
+		}
+	}
+
+	/// <summary>
+	/// Gets or sets the current x tile the running player is on.
+	/// </summary>
+	/// <value>The current y.</value>
+	public int CurrentY
+	{
+		get
+		{
+			return Mathf.RoundToInt(transform.position.y);
+		}
+		set
+		{
+			transform.position = new Vector3((float)value, transform.position.x);
+		}
+	}
+
+	/// <summary>
+	/// The full running speed of the running mouse in tiles per second.
+	/// </summary>
+	public float RunningSpeed = 2f;
+
+	/// <summary>
+	/// 
+	/// </summary>
+	private void FixedUpdate()
+	{
+		GetComponent<Rigidbody2D>().MovePosition(
+						new Vector2((transform.position.x + (Input.GetAxis("Horizontal") * RunningSpeed * Time.deltaTime)),
+				        		    (transform.position.y + (Input.GetAxis("Vertical") * RunningSpeed * Time.deltaTime))));
+	}
+}
