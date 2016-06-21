ddfee662622a86ef4677eb5013c562b380078f1b
Joseph Miller <jmnintendo@gmail.com>
2016-06-21 06:40:41 -0700

    Added basic door opening functionality (including a front door animation). Animation timing is off when closing the Door Animations 256.png(delay of a couple of seconds), though the collider turns on correctly.

---
+++ Assets/_Scripts/StandardDoorBehavior.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Animator))]
+[RequireComponent (typeof(Collider2D))]
+public class StandardDoorBehavior : DoorBehavior
+{
+	private Animator animator = null;
+	private Collider2D doorCollider = null;
+
+	protected override void Init()
+	{
+		animator = GetComponent<Animator>();
+		doorCollider = GetComponent<Collider2D>();
+		animator.SetFloat("AnimChangeMultiplier", 0f);
+		animator.Play("Opening");
+	}
+
+	protected override void Open()
+	{
+		doorCollider.enabled = false;
+		animator.SetFloat("AnimChangeMultiplier", 1f);
+		print( "Open " + doorLevel.ToString() );
+	}
+
+	protected override void Close()
+	{
+		doorCollider.enabled = true;
+		animator.SetFloat("AnimChangeMultiplier", -1f);
+		print( "Close " + doorLevel.ToString() );
+	}
+}
