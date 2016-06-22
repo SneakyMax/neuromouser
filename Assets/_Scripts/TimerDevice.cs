0efa4f718c62ea2f9dfbb585f3004e471fe748a8
Joseph Miller <jmnintendo@gmail.com>
2016-06-22 03:58:37 -0700

    Added Timer prefab and code. Still needs added to interface.

---
+++ Assets/_Scripts/TimerDevice.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerDevice : MonoBehaviour
+{
+	public delegate void OnTimerZero();
+
+	public event OnTimerZero TimerZero;
+
+	public float SecondsLeft = 0f;
+
+	public bool TimerRunning = false;
+
+	public TimerDigit SecondsOnes = null;
+	public TimerDigit SecondsTens = null;
+	public TimerDigit MinutesOnes = null;
+	public TimerDigit MinutesTens = null;
+
+	/// <summary>
+	/// Sets the timer digits based on SecondsLeft.
+	/// </summary>
+	private void SetTimerDigits()
+	{
+		int secondsLeft = Mathf.FloorToInt(SecondsLeft);
+
+		if ( secondsLeft >= 3600 )
+		{
+			SecondsOnes.CurrentDigit = 9;
+			SecondsTens.CurrentDigit = 5;
+			MinutesOnes.CurrentDigit = 9;
+			MinutesTens.CurrentDigit = 5;
+		}
+		else
+		{
+			int minutesLeft = secondsLeft / 60;
+			MinutesTens.CurrentDigit = minutesLeft / 10;
+			MinutesOnes.CurrentDigit = minutesLeft % 10;
+			SecondsTens.CurrentDigit = ( ( secondsLeft % 60 ) / 10 );
+			SecondsOnes.CurrentDigit = ( ( secondsLeft % 60 ) % 10 );
+		}
+	}
+
+	private void Update()
+	{
+		if (TimerRunning)
+		{
+			SecondsLeft -= Time.deltaTime;
+			if ( SecondsLeft < 0f )
+			{
+				SecondsLeft = 0f;
+				TimerRunning = false;
+				if ( TimerZero != null )
+				{
+					TimerZero();
+				}
+			}
+		}
+		SetTimerDigits();
+	}
+}
---
+++ Assets/_Scripts/TimerDigit.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(SpriteRenderer))]
+public class TimerDigit : MonoBehaviour
+{
+	public int CurrentDigit
+	{
+		get
+		{
+			return currentDigit;
+		}
+		set
+		{
+			currentDigit = value;
+			SetSpriteDigit();
+		}
+	}
+
+	public Sprite Digit0Sprite = null;
+	public Sprite Digit1Sprite = null;
+	public Sprite Digit2Sprite = null;
+	public Sprite Digit3Sprite = null;
+	public Sprite Digit4Sprite = null;
+	public Sprite Digit5Sprite = null;
+	public Sprite Digit6Sprite = null;
+	public Sprite Digit7Sprite = null;
+	public Sprite Digit8Sprite = null;
+	public Sprite Digit9Sprite = null;
+	public Sprite DigitEmptySprite = null;
+
+	/// <summary>
+	/// The current digit. If this is anything other than 0-9 it will use DigitEmptySprite.
+	/// </summary>
+	private int currentDigit = -1;
+
+	private void SetSpriteDigit()
+	{
+		switch (currentDigit)
+		{
+			case 0:
+				GetComponent<SpriteRenderer>().sprite = Digit0Sprite;
+				break;
+			case 1:
+				GetComponent<SpriteRenderer>().sprite = Digit1Sprite;
+				break;
+			case 2:
+				GetComponent<SpriteRenderer>().sprite = Digit2Sprite;
+				break;
+			case 3:
+				GetComponent<SpriteRenderer>().sprite = Digit3Sprite;
+				break;
+			case 4:
+				GetComponent<SpriteRenderer>().sprite = Digit4Sprite;
+				break;
+			case 5:
+				GetComponent<SpriteRenderer>().sprite = Digit5Sprite;
+				break;
+			case 6:
+				GetComponent<SpriteRenderer>().sprite = Digit6Sprite;
+				break;
+			case 7:
+				GetComponent<SpriteRenderer>().sprite = Digit7Sprite;
+				break;
+			case 8:
+				GetComponent<SpriteRenderer>().sprite = Digit8Sprite;
+				break;
+			case 9:
+				GetComponent<SpriteRenderer>().sprite = Digit9Sprite;
+				break;
+			default:
+				GetComponent<SpriteRenderer>().sprite = DigitEmptySprite;
+				break;
+		}
+	}
+}
