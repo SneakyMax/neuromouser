56e458b416ab87ea47468e7ec6c96b3526823187
Joseph Miller <jmnintendo@gmail.com>
2016-06-22 10:56:19 -0700

    Exit point added to level editor. Code partially supported (for now it just kills the mouse).

---
+++ Assets/_Scripts/GameObjects/ExitPoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+namespace Assets._Scripts.GameObjects
+{
+	[RequireComponent (typeof(Collider2D))]
+	public class ExitPoint : InGameObject
+	{
+		public override int Layer { get { return 1; } }
+
+		public override bool IsTraversableAt(GridPosition position)
+		{
+			return false;
+		}
+
+		public void OnTriggerEnter2D(Collider2D otherCollider)
+		{
+			GameStateController.Instance.PlayerDied();
+		}
+	}
+}
---
+++ Assets/_Scripts/LevelEditor/Objects/ExitPoint.cs
@@ -0,0 +1,9 @@
+﻿namespace Assets._Scripts.LevelEditor.Objects
+{
+	[UnityComponent]
+	public class ExitPoint : PlacedObject
+	{
+		private static readonly int[] layers = { 1 };
+		public override int[] Layers { get { return layers; } }
+	}
+}
