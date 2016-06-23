8979279aa08030f75123aa7c6d38ddcb7e3a8900
Joseph Miller <jmnintendo@gmail.com>
2016-06-23 16:45:43 -0700

    Adjusted the title screen interface (did what I could) and added an Instructions button.

---
+++ Assets/_Scripts/InstructionButton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets._Scripts
+{
+	public class InstructionButton : MonoBehaviour
+	{
+		[AssignedInUnity]
+		public Sprite InstructionSprite;
+
+		public Image MainImage;
+
+		private Sprite mainSprite;
+
+		private bool showInstructions = false;
+
+		public void InstructionToggle()
+		{
+			if (showInstructions == true)
+			{
+				MainImage.sprite = mainSprite;
+				showInstructions = false;
+			}
+			else
+			{
+				mainSprite = MainImage.sprite;
+				MainImage.sprite = InstructionSprite;
+				showInstructions = true;
+			}
+		}
+	}
+}
